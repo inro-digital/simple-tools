9e79def2fae276596339170f38996af01bd00e00
Ben Pevsner <git@bpev.me>
2024-05-24 16:35:24 -0600

    docs(timers): adds initial timer docs

--- timers/countdown.ts
+++ timers/countdown.ts
@@ -1,20 +1,49 @@
 import State from '../utils/state.ts'
 import { formatDisplayTime } from './utils.ts'
 
+/** State returned via `countdown.state` or `countdown.addEventListener` */
 export interface CountdownState {
+  /* The time formatted as a pretty string */
   display: string
+  /* ms elapsed since the timer started; does not include pause time */
   elapsed: number
+  /* Is true when both `start` and `pause` have been called */
   isPaused: boolean
+  /** Is true when `start` has been called, and `stop` has not been called. */
   isStarted: boolean
+  /* ms remaining before the timer resolves. (total - elapsed) */
   remaining: number
+  /* total ms of the timer will have been counting down */
   total: number
 }
 
 export interface CountdownOptions {
+  /* Number that we count down from */
   initialMS?: number
-  resolutionMS?: number // How often listeners should get called, in ms
+  /* Frequency that listeners are called while the timer is counting */
+  resolutionMS?: number
 }
 
+/**
+ * A timer that counts down from a given number. All public methods trigger
+ * event listeners. Additionally, if the timer is running, event listeners
+ * will trigger every `resolutionMS` interval.
+ *
+ * @example Basic Usage
+ * ```ts
+ * import Countdown, { CountdownState } from '@inro/simple-tools/countdown'
+ * const timer = new Countdown({ initialMS: 70_000 }) // 70s timer
+ * timer.addEventListener((state: CountdownState) => {
+ *   console.log(state.display) // "1:10.0"
+ *   console.log(state.remaining) // 70000
+ * })
+ *
+ * document.getElementById('#start').onclick = () => timer.start()
+ * document.getElementById('#pause').onclick = () => timer.pause()
+ * document.getElementById('#stop').onclick = () => timer.stop()
+ * document.getElementById('#reset').onclick = () => timer.reset()
+ * ```
+ */
 export default class Countdown extends State<CountdownState> {
   #elapsedBeforePauseMS: number
   #initialMS: number
@@ -22,6 +51,7 @@ export default class Countdown extends State<CountdownState> {
   #resolutionMS: number | undefined
   #startMS: number | undefined
 
+  /* Timer resolution defaults to 10ms */
   constructor({ initialMS = 0, resolutionMS = 10 }: CountdownOptions = {}) {
     super({
       display: formatDisplayTime(initialMS),
@@ -36,6 +66,12 @@ export default class Countdown extends State<CountdownState> {
     this.#resolutionMS = resolutionMS
   }
 
+  /**
+   * Starts the timer. If resolutionMS is provided, this also starts calling
+   * listeners on interval. If the time remaining reaches zero, the timer
+   * will automatically run the `.stop()` method (it will, however, not reset
+   * to default automatically).
+   */
   start() {
     // Prevent #startMS update and additional setInterval if already running
     if (this.state.isStarted && !this.state.isPaused) return
@@ -67,6 +103,7 @@ export default class Countdown extends State<CountdownState> {
     }
   }
 
+  /** Pauses the timer, and clears the interval */
   pause() {
     if (!this.state.isStarted || this.state.isPaused) return
     if (!this.#startMS) throw new Error('unexpected no start time')
@@ -85,6 +122,7 @@ export default class Countdown extends State<CountdownState> {
   }
 
   #stop() {
+    if (!this.state.isStarted) return
     clearInterval(this.#interval)
     this.#elapsedBeforePauseMS = 0
     this.#interval = undefined
@@ -94,12 +132,16 @@ export default class Countdown extends State<CountdownState> {
     this.state.isStarted = false
   }
 
+  /** Stops the timer, and clears the interval. It does not reset time. */
   stop() {
-    if (!this.state.isStarted) return
     this.#stop()
     this.notify()
   }
 
+  /**
+   * Resets to initial state. If provided options, it will override the
+   * initial settings
+   */
   reset(options: CountdownOptions = {}) {
     this.#stop()
     this.#initialMS = options?.initialMS ?? this.#initialMS
--- timers/stopwatch.ts
+++ timers/stopwatch.ts
@@ -1,24 +1,60 @@
 import State from '../utils/state.ts'
 import { formatDisplayTime } from './utils.ts'
 
+/** Lap data that is saved as a history. It is deleted on `reset` */
+export interface Lap {
+  /** ms elapsed since the last lap was logged */
+  split: number
+  /** The split time formatted as a pretty string */
+  splitDisplay: string
+  /** ms elapsed since the beginning of the stopwatch (elapsed - prevSplit) */
+  total: number
+  /** The total time formatted as a pretty string */
+  totalDisplay: string
+}
+
+/** State returned via `stopwatch.state` or `stopwatch.addEventListener` */
 export interface StopwatchState {
+  /** The time formatted as a pretty string */
   display: string
+  /** ms elapsed since the timer started; does not include pause time */
   elapsed: number
+  /** Is true when both `start` and `pause` have been called */
   isPaused: boolean
+  /** Is true when `start` has been called, and `stop` has not been called. */
   isStarted: boolean
-  laps: Array<{
-    split: number
-    splitDisplay: string
-    total: number
-    totalDisplay: string
-  }>
+  laps: Array<Lap>
 }
 
 export interface StopwatchOptions {
+  /* Number that we count up from */
   initialMS?: number
-  resolutionMS?: number // How often listeners should get called, in ms
+  /* Frequency that listeners are called while the timer is counting */
+  resolutionMS?: number
 }
 
+/**
+ * A timer that counts up from a given number. All public methods trigger
+ * event listeners. Additionally, if the timer is running, event listeners
+ * will trigger every `resolutionMS` interval.
+ *
+ * @example Basic Usage
+ * ```ts
+ * import Stopwatch, { StopwatchState } from '@inro/simple-tools/stopwatch'
+ * const timer = new Stopwatch()
+ * timer.addEventListener((state: CountdownState) => {
+ *   console.log(state.display) // "0:00.0"
+ *   console.log(state.elapsed) // 0
+ *   console.log(state.laps) // []
+ * })
+ *
+ * document.getElementById('#start').onclick = () => timer.start()
+ * document.getElementById('#pause').onclick = () => timer.pause()
+ * document.getElementById('#stop').onclick = () => timer.stop()
+ * document.getElementById('#reset').onclick = () => timer.reset()
+ * document.getElementById('#lap').onclick = () => timer.lap()
+ * ```
+ */
 export default class Stopwatch extends State<StopwatchState> {
   #elapsedBeforePauseMS: number
   #initialMS: number
@@ -26,6 +62,7 @@ export default class Stopwatch extends State<StopwatchState> {
   #resolutionMS: number | undefined
   #startMS: number | undefined
 
+  /* Timer resolution defaults to 10ms */
   constructor({ initialMS = 0, resolutionMS = 10 }: StopwatchOptions = {}) {
     super({
       display: formatDisplayTime(initialMS),
@@ -39,6 +76,10 @@ export default class Stopwatch extends State<StopwatchState> {
     this.#resolutionMS = resolutionMS
   }
 
+  /**
+   * Starts the timer. If resolutionMS is provided, this also starts calling
+   * listeners on interval.
+   */
   start() {
     // Prevent #startMS update and additional setInterval if already running
     if (this.state.isStarted && !this.state.isPaused) return
@@ -62,6 +103,7 @@ export default class Stopwatch extends State<StopwatchState> {
     }
   }
 
+  /** Pauses the timer, and clears the interval */
   pause() {
     if (!this.state.isStarted || this.state.isPaused) return
     if (!this.#startMS) throw new Error('unexpected no start time')
@@ -91,11 +133,16 @@ export default class Stopwatch extends State<StopwatchState> {
     this.state.isStarted = false
   }
 
+  /** Stops the timer, and clears the interval. It does not reset time. */
   stop() {
     this.#stop()
     this.notify()
   }
 
+  /**
+   * Resets to initial state. If provided options, it will override the
+   * initial settings
+   */
   reset(options: StopwatchOptions = {}) {
     this.#stop()
     this.#initialMS = options.initialMS ?? this.#initialMS
@@ -110,6 +157,7 @@ export default class Stopwatch extends State<StopwatchState> {
     this.notify()
   }
 
+  /** Logs a lap. This does not affect timer counting at all. */
   lap() {
     const total = this.state.elapsed
     const prevTime = this.state.laps[0]?.total ?? 0
